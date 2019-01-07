60efa2f83c36fc84bdf22f4cce520bc134bc86a0
ssarif <sarifr55@gmail.com>
2019-01-07 19:47:03 +0530

    v0.2

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
+
 from flask import Flask 
 
 app = Flask(__name__)
+guesses = ["Python", "Java", "C++"]
+
 
 @app.route("/")
 def index():
 	return "<h1>Guess the language</h1>"
 
+
+@app.route("/guess/<int:id>")
+def guess(id):
+	return ("<h1>Guess the language!</h1>"
+		     "<p>My guess: {0}</p>").format(guesses[id])
 
 if __name__=="__main__":
 	app.run(debug=True)
